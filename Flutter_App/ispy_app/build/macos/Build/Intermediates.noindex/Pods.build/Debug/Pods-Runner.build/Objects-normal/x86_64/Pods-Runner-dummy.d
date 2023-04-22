1da655c39bae0a4e3531e76631d7839f7ab1c6dd
Jaz <barnett.jazmine2000@gmail.com>
2023-04-22 00:03:45 -0400

    uhh is this working

---
+++ Flutter_App/ispy_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods-Runner-dummy.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/jazminebarnett/Flutter_App/ispy_app/macos/Pods/Target\ Support\ Files/Pods-Runner/Pods-Runner-dummy.m \
+  /Users/jazminebarnett/Flutter_App/ispy_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/module.modulemap
---
+++ Flutter_App/ispy_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/Objects-normal/x86_64/Pods_Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/jazminebarnett/Flutter_App/ispy_app/build/macos/Build/Intermediates.noindex/Pods.build/Debug/Pods-Runner.build/DerivedSources/Pods_Runner_vers.c
